4220c9513b71826c87fc85c4f96dd8463952189c
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-15 13:00:32 +0100

    Small refactoring

--- src/main/java/de/sfuhrm/args2all/mapping/Mapper.java
+++ src/main/java/de/sfuhrm/args2all/mapping/Mapper.java
@@ -2,7 +2,9 @@ package de.sfuhrm.args2all.mapping;
 
 import lombok.Getter;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,29 +16,30 @@ import java.util.stream.Collectors;
  * */
 public class Mapper {
 
-    /** The list of mappers. */
-    private final List<Mapping> mappers;
+    /** The list of mappings. */
+    private final List<Mapping> mappings;
 
     /** Associative map with the key being the
      * annotation class and the value being the mapping
      * itself.
      * */
     @Getter
-    private final Map<Class<?>, Mapping> classMappings;
+    private final Map<Class<? extends Annotation>, Mapping> classMappings;
 
     /** Creates a new mapper instance with the default mappings
      * installed.
      * */
     public Mapper() {
-        mappers = Arrays.asList(
+        mappings = Arrays.asList(
                 new Args4jArgumentMapping(),
                 new Args4jOptionMapping(),
                 new JCommanderParameterMapping()
         );
 
-        classMappings = mappers
+        Map<Class<? extends Annotation>, Mapping> map = mappings
                 .stream()
                 .collect(Collectors.toMap(
                         p -> p.getAnnotationClass(), p -> p));
+        classMappings = Collections.unmodifiableMap(map);
     }
 }
